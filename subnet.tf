9be3db35b9a0bca4c8357b3fb2fd1d682b3a5e91
Sharanjeet Kaur <sharanjeetaulakh@gmail.com>
2026-06-05 01:57:30 +0530

    Implement subnet creation using for_each loop

--- subnet.tf
+++ subnet.tf
@@ -4,4 +4,4 @@ resource "azurerm_subnet" "subnet_section" {
   resource_group_name=azurerm_resource_group.resource_group.name
 virtual_network_name=azurerm_virtual_network.vnet.name
 address_prefixes=[each.value]
-}
+} 
