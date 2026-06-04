8871e93831e053eaa7ba56b9cb231d9220d28116
Sharanjeet Kaur <sharanjeetaulakh@gmail.com>
2026-06-05 01:56:13 +0530

    Create Azure Virtual Network using Terraform

--- virtual.tf
+++ virtual.tf
@@ -3,4 +3,4 @@ resource "azurerm_virtual_network" "vnet"{
     location=azurerm_resource_group.resource_group.location
     resource_group_name=azurerm_resource_group.resource_group.name
     address_space=["192.168.0.0/16"]
-}
+} 
